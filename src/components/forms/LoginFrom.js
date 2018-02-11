ce7ca7a3a540ba6b7e05acb21e40ccc0be8d47e2
men.innocence@gmail.com <men.innocence@gmail.com>
2018-02-11 18:56:17 +0700

    CheckLogin

--- src/components/forms/LoginFrom.js
+++ src/components/forms/LoginFrom.js
@@ -4,40 +4,74 @@ import {
     TextInput, Button
 } from 'react-native';
 
+import Validator from 'validator';
+
 class LoginFrom extends Component {
     constructor(props) {
         super(props);
         this.state = {
             email: '', 
             password: '',
+            errors: {
+                email: '',
+                password: ''
+            }
         }; // Component ของใครของมัน
         this.onSubmit = this.onSubmit.bind(this);
         this.onChange = this.onChange.bind(this);
+        this.checkEmail = this.checkEmail.bind(this);
+        this.checkPassword = this.checkPassword.bind(this);
     }
     onChange = (email) => {
         console.log(email);
-        this.setState({email})
+        this.setState({email});
+    }
 
+    checkEmail = () => {
+        console.log('Email', this.state.email);
+        const { email } = this.state.errors;
+        if(!Validator.isEmail(this.state.email))
+            this.setState({});
+        else
+            this.setState({...this.state, errors: {email: '', password:''}});
     }
+
+    checkPassword = () => {
+        
+    }
+
     onSubmit = () => {
-        console.log('OnSubmit', this.state);
+        // console.log('OnSubmit', this.state);
+        const {errors,password,email} = this.state
+        if(
+            errors.email == '' && errors.password == '' &&
+            email !== '' && password ==''
+        ){
+            console.log('Go to API');
+        }
+        // console.log('OnSubmit', this.state);
     }
+
     render() {
         return (
             <View style= {{ padding: 10 }}>
                 <Text>Login</Text>
                 <TextInput
-                    placeholder = 'Enter Username'
+                    placeholder = 'Exemple@excemple.com'
                     style = {{paddingTop: 5}}
-                    onChangeText = {(email) => this.setState({email})}
+                    onChangeText = {(email) => this.setState({email : email.toLowerCase()})}
                     value = {this.state.email}
+                    onBlur={this.checkEmail}
                 />
+                <Text style = {{color: 'red', paddingLeft: 5}}
+                >{this.state.errors.email}</Text>
                 <TextInput
                     placeholder = 'Enter Password'
                     style = {{paddingTop: 5}}
                     onChangeText = {(password) => this.setState({password})}
                     value = {this.state.password}
                     secureTextEntry
+                    onBlur = {this.checkPassword}
                 />
                 <Button
                     title = 'Login'
